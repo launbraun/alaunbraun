cfb1f168f6566dc667bc4851317f4c3d7d32895c
launbraun <launbraun@bk.ru>
2018-03-06 20:25:37 +0300

    1.3 JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,8 +1,20 @@
 package ru.job4j;
 
+/**
+* Класс Калькулирует =) 
+*
+* @author Alesksand Launbraun
+* @since 06.03.2018
+* @version 1.0
+*/ 
 
 public class Calculate {
-   
+	
+    /**
+    * Вывод строки в консоль
+	* @param args - args
+	*/
+	
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
