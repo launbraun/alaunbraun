016a77de7cc1be5d04554313867873a00f7bb5d5
launbraun <launbraun@bk.ru>
2018-03-08 15:56:02 +0300

    1.4 JUnit

---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,32 @@
+package ru.job4j;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test.
+* Класс Калькулирует =) 
+*
+* @author Alesksand Launbraun
+* @since 06.03.2018
+* @version 1.0
+*/ 
+
+public class CalculateTest {
+    /**
+     * Test add.
+     */
+    @Test
+
+public void whenTakeNameThenTreeEchoPlusName() {
+    String input = "Alesksand Launbraun";
+    String expect = "Echo, echo, echo : Alesksand Launbraun"; 
+    Calculate calc = new Calculate();
+    String result = calc.echo(input);
+    assertThat(result, is(expect));
+}
+ 
+}
