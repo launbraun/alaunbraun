baa6a810d3e059c87ab290c249d2462d2e3278fc
launbraun <launbraun@bk.ru>
2018-03-10 14:03:39 +0300

    text

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,32 +1,32 @@
 package ru.job4j;
 
 /**
-* Класс Калькулирует =) 
-*
-* @author Alesksand Launbraun
-* @since 06.03.2018
-* @version 1.0
-*/ 
+ * Класс Калькулирует =)
+ *
+ * @author Alesksand Launbraun
+ * @since 06.03.2018
+ * @version 1.0
+ */
 
 public class Calculate {
-	
-    /**
-    * Вывод строки в консоль
-	* @param args - args
-	*/
-	
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-	}	
-	
+
 	/**
-	* Method echo.
-	* @param name Alesksand Launbraun.
-	* @return Echo plus Alesksand Launbraun.
-	*/
-	
-public String echo(String name) {
-    return "Echo, echo, echo : " + name;
+	 * Вывод строки в консоль
+	 * @param args - args
+	 */
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
 	}
-  }
+
+	/**
+	 * Method echo.
+	 * @param name Alesksand Launbraun.
+	 * @return Echo plus Alesksand Launbraun.
+	 */
+
+	public String echo(String name) {
+		return "Echo, echo, echo : " + name;
+	}
+}
 
--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -7,13 +7,13 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test.
-* Класс Калькулирует =) 
-*
-* @author Alesksand Launbraun
-* @since 06.03.2018
-* @version 1.0
-*/ 
+ * Test.
+ * Класс Калькулирует =)
+ *
+ * @author Alesksand Launbraun
+ * @since 06.03.2018
+ * @version 1.0
+ */
 
 public class CalculateTest {
     /**
@@ -21,12 +21,12 @@ public class CalculateTest {
      */
     @Test
 
-public void whenTakeNameThenTreeEchoPlusName() {
-    String input = "Alesksand Launbraun";
-    String expect = "Echo, echo, echo : Alesksand Launbraun"; 
-    Calculate calc = new Calculate();
-    String result = calc.echo(input);
-    assertThat(result, is(expect));
-	}
- 
+    public void whenTakeNameThenTreeEchoPlusName() {
+        String input = "Alesksand Launbraun";
+        String expect = "Echo, echo, echo : Alesksand Launbraun";
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
+
 }
