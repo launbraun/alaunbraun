c0c54d44ca36b050d16d14a781cac4abf180d4b7
launbraun <launbraun@bk.ru>
2018-03-11 10:05:04 +0300

    3.1. Глупый бот.

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,28 @@
+package ru.job4j.condition;
+
+/**
+ * Глупый бот отвечает на вопросы
+ *
+ * @author Alesksand Launbraun
+ * @since 11.03.2018
+ * @version 1.0
+ */
+
+public class DummyBot {
+    /**
+     * Отвечает на вопросы.
+     * @param question Вопрос от клиента.
+     * @return Ответ.
+     */
+
+    public String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Спросите другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            rsl = "Привет, умник.";
+        } else if ("Пока.".equals(question)) {
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+}
+
