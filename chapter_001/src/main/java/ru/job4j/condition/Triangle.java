3a6030e39f21d656b8ae74b65d6f6bfa83567c7a
launbraun <launbraun@bk.ru>
2018-03-11 13:05:41 +0300

    3.3. Вычисление площади треугольника

---
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -0,0 +1,64 @@
+package ru.job4j.condition;
+
+/**
+ * Класс вычисляет площадь треугольника
+ *
+ * @author Alesksand Launbraun
+ * @since 11.03.2018
+ * @version 1.0
+ */
+
+public class Triangle {
+    private Point a;
+    private Point b;
+    private Point c;
+
+    public Triangle(Point a, Point b, Point c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    /**
+     * Метод вычисления полупериметра по длинам сторон.
+     * Формула (ab + ac + bc) / 2
+     * @param ab расстояние между точками a b
+     * @param ac расстояние между точками a c
+     * @param bc расстояние между точками b c
+     * @return Перимент.
+     */
+    public double period(double ab, double ac, double bc) {
+        return (ab + ac + bc) / 2;
+    }
+
+    /**
+     * Метод вычисляет площадь треугольника
+     * @return возвращает площадь, если треугольник существует или -1, если треугольника нет.
+     */
+    public double area() {
+        double rsl = -1;
+        double ab = this.a.distanceTo(this.b);
+        double ac = this.a.distanceTo(this.c);
+        double bc = this.b.distanceTo(this.c);
+        double p = this.period(ab, ac, bc);
+        if (this.exist(ab, ac, bc)) {
+            rsl = Math.sqrt(p * (p - ab) * (p - ac) * (p - bc));
+        }
+        return rsl;
+    }
+
+    /**
+     * Метод проверяет можно ли построить треугольник с такими длинами сторон.
+     * @param ab Длина от точки a b.
+     * @param ac Длина от точки a c.
+     * @param bc Длина от точки b c.
+     * @return true или false
+     */
+    private boolean exist(double ab, double ac, double bc) {
+        if (Math.abs(ab) == 0 || Math.abs(ac) == 0 || Math.abs(bc) == 0)  {
+            return false;
+        }
+        return true;
+    }
+}
+
